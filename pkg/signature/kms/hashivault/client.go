56a91f560f4477e0755fb5e4a1034d9fb42edace
LeSuisse <thomas@gerbet.me>
2021-07-29 23:13:20 +0200

    Transit backend path is hardcoded for some operations of the KMS Vault client (#102)
    The environment variable TRANSIT_SECRET_ENGINE_PATH is not always used.
    Some operations are hardcoded to "transit/". Users with a transit
    backend mounted with a non default path might not be able to use it.
    
    Signed-off-by: Thomas Gerbet <thomas.gerbet@enalean.com>

--- pkg/signature/kms/hashivault/client.go
+++ pkg/signature/kms/hashivault/client.go
@@ -179,7 +179,7 @@ func (h *hashivaultClient) public() (crypto.PublicKey, error) {
 func (h hashivaultClient) sign(digest []byte, alg crypto.Hash) ([]byte, error) {
 	client := h.client.Logical()
 
-	signResult, err := client.Write(fmt.Sprintf("/transit/sign/%s%s", h.keyPath, hashString(alg)), map[string]interface{}{
+	signResult, err := client.Write(fmt.Sprintf("/%s/sign/%s%s", h.transitSecretEnginePath, h.keyPath, hashString(alg)), map[string]interface{}{
 		"input":     base64.StdEncoding.Strict().EncodeToString(digest),
 		"prehashed": alg != crypto.Hash(0),
 	})
@@ -249,7 +249,7 @@ func hashString(h crypto.Hash) string {
 func (h hashivaultClient) createKey(typeStr string) (crypto.PublicKey, error) {
 	client := h.client.Logical()
 
-	if _, err := client.Write(fmt.Sprintf("/transit/keys/%s", h.keyPath), map[string]interface{}{
+	if _, err := client.Write(fmt.Sprintf("/%s/keys/%s", h.transitSecretEnginePath, h.keyPath), map[string]interface{}{
 		"type": typeStr,
 	}); err != nil {
 		return nil, errors.Wrap(err, "Failed to create transit key")
