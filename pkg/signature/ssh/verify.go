40ab265d1d55a460fa0bf9af78931f108771629d
Dan Lorenc <lorenc.d@gmail.com>
2021-11-14 10:35:05 -0600

    Move the ssh signing/verification utilities to sigstore from rekor.
    SSH signing has started to gain momentum with support in git! We have an implementation
    in rekor that works, but it probably doesn't belong there. Let's move it here and realign
    it with our sigstore signing APIs.
    
    Signed-off-by: Dan Lorenc <lorenc.d@gmail.com>

---
+++ pkg/signature/ssh/verify.go
@@ -0,0 +1,59 @@
+//
+// Copyright 2021 The Sigstore Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ssh
+
+import (
+	"io"
+	"io/ioutil"
+
+	"github.com/sigstore/sigstore/pkg/signature"
+	"golang.org/x/crypto/ssh"
+)
+
+// Verify verifies the supplied signature against the specified key.
+func Verify(message io.Reader, armoredSignature []byte, pubKey ssh.PublicKey) error {
+	decodedSignature, err := Decode(armoredSignature)
+	if err != nil {
+		return err
+	}
+
+	// Hash the message so we can verify it against the signature.
+	h := supportedHashAlgorithms[decodedSignature.hashAlg]()
+	if _, err := io.Copy(h, message); err != nil {
+		return err
+	}
+	hm := h.Sum(nil)
+
+	toVerify := messageWrapper{
+		Namespace:     "file",
+		HashAlgorithm: decodedSignature.hashAlg,
+		Hash:          string(hm),
+	}
+	signedMessage := ssh.Marshal(toVerify)
+	signedMessage = append([]byte(magicHeader), signedMessage...)
+	return pubKey.Verify(signedMessage, decodedSignature.signature)
+}
+
+var _ signature.Verifier = (*Signer)(nil)
+
+// VerifySignature verifies a suppled signature.
+func (s *Signer) VerifySignature(signature, message io.Reader, opts ...signature.VerifyOption) error {
+	b, err := ioutil.ReadAll(signature)
+	if err != nil {
+		return err
+	}
+	return Verify(message, b, s.signer.PublicKey())
+}
