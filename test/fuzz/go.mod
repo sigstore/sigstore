979d458011e4e71c186bf85a655e8ad88da4ffa6
naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-11-16 17:13:16 +0000

    Feat : Fuzzing
    Enabling fuzzing for sigstore.
    
    The first steps into fuzzing Sigstore.
    The goal is to integrate this into oss-fuzz using libfuzzer
    https://google.github.io/oss-fuzz/getting-started/new-project-guide/go-lang/ and
    https://security.googleblog.com/2021/11/clusterfuzzlite-continuous-fuzzing-for.html
    
    Signed-off-by: naveen <172697+naveensrinivasan@users.noreply.github.com>

---
+++ test/fuzz/go.mod
@@ -0,0 +1,11 @@
+module github.com/sigstore/sigstore/test/fuzz
+
+go 1.16
+
+require (
+	github.com/AdaLogics/go-fuzz-headers v0.0.0-20211102141018-f7be0cbad29c
+	github.com/dvyukov/go-fuzz v0.0.0-20210914135545-4980593459a1
+	github.com/elazarl/go-bindata-assetfs v1.0.1 // indirect
+	github.com/sigstore/sigstore v1.0.1
+	github.com/stephens2424/writerset v1.0.2 // indirect
+)
---
+++ test/fuzz/pem/fuzzcert.go
@@ -0,0 +1,46 @@
+//go:build gofuzz
+// +build gofuzz
+//
+// Copyright 2021 The Sigstore Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fuzz
+
+import (
+	"bytes"
+	"encoding/pem"
+	"fmt"
+
+	"github.com/sigstore/sigstore/pkg/cryptoutils"
+)
+
+func FuzzLoadCertificates(data []byte) int {
+	b, _ := pem.Decode(data)
+	if b == nil {
+		return 0
+	}
+	result, err := cryptoutils.LoadCertificatesFromPEM(bytes.NewReader(data))
+	if err != nil {
+		if result != nil {
+			panic(fmt.Sprintf("result %v should be nil when there is an error %v", result, err))
+		}
+		return 0
+	}
+	for _, cert := range result {
+		if len(cert.Raw) == 0 {
+			panic(fmt.Sprintf("x509 cert raw is empty"))
+		}
+	}
+	return 1
+}
