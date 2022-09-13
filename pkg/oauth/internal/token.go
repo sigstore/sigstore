fe89132b9369fc783254378ff8cfa68edfb64f72
Hayden B <hblauzvern@google.com>
2022-09-13 17:14:47 +0100

    Package descriptions for all TODOs (#685)
    Signed-off-by: Hayden Blauzvern <hblauzvern@google.com>
    
    Signed-off-by: Hayden Blauzvern <hblauzvern@google.com>

--- pkg/oauth/internal/token.go
+++ pkg/oauth/internal/token.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package internal TODO: add meaningfull description
+// Package internal includes functions to support OAuth/OIDC
 package internal
 
 import (
