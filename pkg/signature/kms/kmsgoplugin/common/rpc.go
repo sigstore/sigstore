c8acbfcbed7679c5de15e717e06f5115ae2c056a
Ramon Petgrave <ramon.petgrave64@gmail.com>
2024-10-28 16:00:42 -0400

    refactor client and server creation in test
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

--- pkg/signature/kms/kmsgoplugin/common/rpc.go
+++ pkg/signature/kms/kmsgoplugin/common/rpc.go
@@ -18,11 +18,11 @@
 package common
 
 import (
+	"bytes"
 	"context"
 	"crypto"
 	"fmt"
 	"io"
-	"log/slog"
 	"net/rpc"
 	"time"
 
@@ -37,6 +37,7 @@ import (
 type SignerVerifierRPC struct {
 	KMSGoPluginSignerVerifier
 	client *rpc.Client
+	state  *KMSGoPluginState
 }
 
 type SignerVerifierRPCServer struct {
@@ -106,6 +107,7 @@ func (s *SignerVerifierRPCServer) SetState(args SetStateArgs, resp *SetStateResp
 }
 
 func (c *SignerVerifierRPC) SetState(state *KMSGoPluginState) {
+	c.state = state
 	args := SetStateArgs{State: *state}
 	var resp DefaultAlgorithmResp
 	if err := c.client.Call("Plugin.SetState", args, &resp); err != nil {
@@ -127,19 +129,16 @@ type CreateKeyResp struct {
 
 // CreateKey returns a crypto.PublicKey.
 func (s *SignerVerifierRPCServer) CreateKey(args *CreateKeyArgs, resp *CreateKeyResp) error {
-	// slog.Info("createley", "ctxdeadline", args.CtxDeadline, "glorithm", args.Algorithm)
 	ctx := context.Background()
 	if args.CtxDeadline != nil {
 		var cancel context.CancelFunc
 		ctx, cancel = context.WithDeadline(ctx, *args.CtxDeadline)
 		defer cancel()
 	}
-	slog.Info("servercreatekey", "ctx", ctx, "algorithm", args.Algorithm, "impl", s.Impl)
 	pubKey, err := s.Impl.CreateKey(ctx, args.Algorithm)
 	if err != nil {
 		return err
 	}
-	slog.Info("servercreatekey", "pubkey", pubKey)
 	resp.PublicKey = PublicKeyGobWrapper{PublicKey: pubKey}
 	return nil
 }
@@ -191,7 +190,6 @@ func (s *SignerVerifierRPCServer) SignMessage(args SignMessageArgs, resp *SignMe
 	if args.Opts.Digest != nil && len(args.Opts.Digest) != 0 {
 		opts = append(opts, options.WithDigest(args.Opts.Digest))
 	}
-	// slog.Info("opts", "deadline", args.Opts.CtxDeadline, "hash", args.Opts.Hash.Hash, "digest", args.Opts.Digest)
 
 	// signature, err := s.Impl.SignMessage(args.Message, args.Opts...)
 	signature, err := s.Impl.SignMessage(args.Message, opts...)
@@ -281,7 +279,6 @@ type VerifySignatureResp struct {
 
 // SignMessage signs the provided message.
 func (s *SignerVerifierRPCServer) VerifySignature(args VerifySignatureArgs, resp *VerifySignatureResp) error {
-	// slog.Info("verifySig", "message", args.Message)
 	if err := s.Impl.VerifySignature(args.Signature, args.Message, args.Opts...); err != nil {
 		return err
 	}
@@ -337,37 +334,43 @@ func (c *SignerVerifierRPC) PublicKey(opts ...signature.PublicKeyOption) (crypto
 
 // CryptoSignerArgs contains the args for CryptoSigner().
 type CryptoSignerArgs struct {
-	// Ctx     context.Context
-	// ErrFunc func(error)
 }
 
 // CryptoSignerResp contains the return values for CryptoSigner().
 type CryptoSignerResp struct {
-	// Signer     CryptoSignerGobWrapper
-	// SignerOpts crypto.SignerOpts
 }
 
 // CryptoSigner is not implemented becuase it is not needed by cosign CLI.
 func (s *SignerVerifierRPCServer) CryptoSigner(args CryptoSignerArgs, resp *CryptoSignerResp) error {
-	return fmt.Errorf("%w: CryptoSigner is not implemented for the plugin", ErrorNotImplemented)
-	// signer, signerOpts, err := s.Impl.CryptoSigner(context.Background(), func(err error) { slog.Error((err.Error())) })
-	// if err != nil {
-	// 	return err
-	// }
-	// resp.Signer = CryptoSignerGobWrapper{Signer: signer}
-	// resp.SignerOpts = signerOpts
-	return nil
+	return fmt.Errorf("%w: CryptoSigner is not implemented for the plugin server", ErrorNotImplemented)
 }
 
 // CryptoSigner is not implemented becuase it is not needed by cosign CLI.
 func (c *SignerVerifierRPC) CryptoSigner(ctx context.Context, errFunc func(error)) (crypto.Signer, crypto.SignerOpts, error) {
-	return nil, nil, fmt.Errorf("%w: CryptoSigner is not implemented for the plugin", ErrorNotImplemented)
-	// args := CryptoSignerArgs{}
-	// var resp CryptoSignerResp
-	//
-	//	if err := c.client.Call("Plugin.CryptoSigner", args, &resp); err != nil {
-	//		return nil, nil, err
-	//	}
-	//
-	// return resp.Signer, resp.SignerOpts, nil
+	cryptoSigner := CryptoSignerRPCWrapper{
+		SignerVerifierRPC: c,
+		errFunc:           errFunc,
+		ctx:               ctx,
+	}
+	signerOpts := c.state.HashFunc
+	return cryptoSigner, signerOpts, nil
+}
+
+type CryptoSignerRPCWrapper struct {
+	crypto.Signer
+	*SignerVerifierRPC
+	errFunc func(error)
+	ctx     context.Context
+}
+
+func (c CryptoSignerRPCWrapper) Public() crypto.PublicKey {
+	publicKey, err := c.SignerVerifierRPC.PublicKey()
+	if err != nil {
+		panic(err)
+	}
+	return publicKey
+}
+
+func (c CryptoSignerRPCWrapper) Sign(rand io.Reader, digest []byte, opts crypto.SignerOpts) (signature []byte, err error) {
+	return c.SignerVerifierRPC.SignMessage(io.NopCloser(bytes.NewReader(digest)))
 }
--- pkg/signature/kms/kmsgoplugin/common/rpc_test.go
+++ pkg/signature/kms/kmsgoplugin/common/rpc_test.go
@@ -6,7 +6,7 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"fmt"
-	"log/slog"
+	"os"
 	"testing"
 	"time"
 
@@ -15,9 +15,11 @@ import (
 )
 
 var (
-	testDeadLine  = time.Now().Add(5 * time.Minute)
-	testAlgorithm = "test-algorithm"
-	testPublicKey crypto.PublicKey
+	testDeadLine                  = time.Now().Add(5 * time.Minute)
+	testAlgorithm                 = "test-algorithm"
+	testPublicKey                 crypto.PublicKey
+	testKMSGoPluginSignerVerifier *SignerVerifierRPC
+	testCtx                       context.Context
 )
 
 type TestSignerVerifierImpl struct {
@@ -25,7 +27,6 @@ type TestSignerVerifierImpl struct {
 }
 
 func (TestSignerVerifierImpl) CreateKey(ctx context.Context, algorithm string) (crypto.PublicKey, error) {
-	slog.Info("impl", "ctx", ctx, "algorithm", algorithm)
 	deadline, ok := ctx.Deadline()
 	if !ok {
 		return nil, fmt.Errorf("expected a deadline")
@@ -39,18 +40,20 @@ func (TestSignerVerifierImpl) CreateKey(ctx context.Context, algorithm string) (
 	return testPublicKey, nil
 }
 
-func Test_Connection(t *testing.T) {
+func TestMain(m *testing.M) {
+	// Perform global setup here
+
 	// prepare testPublicKey
 	priv, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
-		t.Fatalf("rsa.GenerateKey failed: %v", err)
+		panic(fmt.Sprintf("rsa.GenerateKey failed: %v", err))
 	}
-	// slog.Info("priv", "priv", priv)
 	testPublicKey = priv.Public()
 
-	ctx, cancel := context.WithCancel(context.Background())
-
+	// prepare the server and client
 	// server and channels to receive the reattach config.
+	ctx, cancel := context.WithCancel(context.Background())
+	testCtx = ctx
 	reattachConfigCh := make(chan *plugin.ReattachConfig, 1)
 	closeCh := make(chan struct{})
 	go plugin.Serve(&plugin.ServeConfig{
@@ -64,33 +67,40 @@ func Test_Connection(t *testing.T) {
 			CloseCh:          closeCh,
 		},
 	})
-
 	var reattachConfig *plugin.ReattachConfig
 	reattachConfig = <-reattachConfigCh
-
-	// connect.
 	client := plugin.NewClient(&plugin.ClientConfig{
 		Cmd:             nil,
 		HandshakeConfig: HandshakeConfig,
 		Plugins:         PluginMap,
 		Reattach:        reattachConfig,
 	})
-
-	// exttact the interface.
 	rpcClient, err := client.Client()
 	if err != nil {
-		t.Fatalf("could not get client: %v", err)
+		panic(fmt.Sprintf("could not get client: %v", err))
 	}
 	raw, err := rpcClient.Dispense(KMSPluginName)
 	if err != nil {
-		t.Fatalf("ould not dispense: %v", err)
+		panic(fmt.Sprintf("ould not dispense: %v", err))
 	}
-	signerVerifier := raw.(*SignerVerifierRPC)
-	slog.Info("sv", "val", signerVerifier)
+	testKMSGoPluginSignerVerifier = raw.(*SignerVerifierRPC)
 
-	// test the interface.
+	exitCode := m.Run() // Run all tests
+
+	client.Kill()
+	cancel()
+	<-closeCh
+
+	// Perform global teardown here
+
+	os.Exit(exitCode)
+}
+
+func Test_CreateKey(t *testing.T) {
+	ctx := testCtx
+	signerVerifier := testKMSGoPluginSignerVerifier
 	wantedPublicKey := testPublicKey
-	ctx, _ = context.WithDeadline(ctx, testDeadLine)
+	ctx, cancel := context.WithDeadline(ctx, testDeadLine)
 	gotPublicKey, err := signerVerifier.CreateKey(ctx, testAlgorithm)
 	if err != nil {
 		t.Fatalf("could not get public key: %v", err)
@@ -98,77 +108,20 @@ func Test_Connection(t *testing.T) {
 	if diff := cmp.Diff(wantedPublicKey, gotPublicKey); diff != "" {
 		t.Errorf("PublicKey mismatch (-want +got):\n%s", diff)
 	}
-	slog.Info("done", "sv", signerVerifier)
-	client.Kill()
 	cancel()
-	<-closeCh
 }
 
-// func TestServer_testMode_AutoMTLS(t *testing.T) {
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	defer cancel()
-
-// 	closeCh := make(chan struct{})
-// 	go Serve(&ServeConfig{
-// 		HandshakeConfig: testVersionedHandshake,
-// 		VersionedPlugins: map[int]PluginSet{
-// 			2: testGRPCPluginMap,
-// 		},
-// 		GRPCServer: DefaultGRPCServer,
-// 		Logger:     hclog.NewNullLogger(),
-// 		Test: &ServeTestConfig{
-// 			Context:          ctx,
-// 			ReattachConfigCh: nil,
-// 			CloseCh:          closeCh,
-// 		},
-// 	})
-
-// 	// Connect!
-// 	process := helperProcess("test-mtls")
-// 	c := NewClient(&ClientConfig{
-// 		Cmd:             process,
-// 		HandshakeConfig: testVersionedHandshake,
-// 		VersionedPlugins: map[int]PluginSet{
-// 			2: testGRPCPluginMap,
-// 		},
-// 		AllowedProtocols: []Protocol{ProtocolGRPC},
-// 		AutoMTLS:         true,
-// 	})
-// 	client, err := c.Client()
-// 	if err != nil {
-// 		t.Fatalf("err: %s", err)
-// 	}
-
-// 	// Pinging should work
-// 	if err := client.Ping(); err != nil {
-// 		t.Fatalf("should not err: %s", err)
-// 	}
-
-// 	// Grab the impl
-// 	raw, err := client.Dispense("test")
-// 	if err != nil {
-// 		t.Fatalf("err should be nil, got %s", err)
-// 	}
-
-// 	tester, ok := raw.(testInterface)
-// 	if !ok {
-// 		t.Fatalf("bad: %#v", raw)
-// 	}
-
-// 	n := tester.Double(3)
-// 	if n != 6 {
-// 		t.Fatal("invalid response", n)
-// 	}
-
-// 	// ensure we can make use of bidirectional communication with AutoMTLS
-// 	// enabled
-// 	err = tester.Bidirectional()
-// 	if err != nil {
-// 		t.Fatal("invalid response", err)
-// 	}
-
-// 	c.Kill()
-// 	// Canceling should cause an exit
-// 	cancel()
-// 	<-closeCh
-// }
+func Test_CreateKey2(t *testing.T) {
+	ctx := testCtx
+	signerVerifier := testKMSGoPluginSignerVerifier
+	wantedPublicKey := testPublicKey
+	ctx, cancel := context.WithDeadline(ctx, testDeadLine)
+	gotPublicKey, err := signerVerifier.CreateKey(ctx, testAlgorithm)
+	if err != nil {
+		t.Fatalf("could not get public key: %v", err)
+	}
+	if diff := cmp.Diff(wantedPublicKey, gotPublicKey); diff != "" {
+		t.Errorf("PublicKey mismatch (-want +got):\n%s", diff)
+	}
+	cancel()
+}
