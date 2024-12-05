302fc1b52cbd08118cdb9223ea6be348a5021ccb
Ramon Petgrave <ramon.petgrave64@gmail.com>
2024-12-05 13:23:49 -0500

    cleanup example, reorganize
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

---
+++ pkg/signature/kms/cliplugin/client.go
@@ -0,0 +1,41 @@
+package cliplugin
+
+import (
+	"context"
+	"crypto"
+	"fmt"
+	"strings"
+
+	"github.com/sigstore/sigstore/pkg/signature"
+	"github.com/sigstore/sigstore/pkg/signature/kms"
+	sigkms "github.com/sigstore/sigstore/pkg/signature/kms"
+	"github.com/sigstore/sigstore/pkg/signature/kms/cliplugin/common"
+)
+
+const (
+	ReferenceScheme = "sigstore-kms-"
+)
+
+func init() {
+	kms.AddProvider(ReferenceScheme, func(ctx context.Context, keyResourceID string, hashFunc crypto.Hash, opts ...signature.RPCOption) (kms.SignerVerifier, error) {
+		return LoadSignerVerifier(ctx, keyResourceID, hashFunc)
+	})
+}
+
+func LoadSignerVerifier(ctx context.Context, inputKeyresourceID string, hashFunc crypto.Hash) (sigkms.SignerVerifier, error) {
+	parts := strings.SplitN(inputKeyresourceID, "://", 2)
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("%w: expected format: [binary name]://[key ref], got: %s", ErrorParsingPluginBinaryName, inputKeyresourceID)
+	}
+	executable, keyResourceID := parts[0], parts[1]
+	InitOptions := &common.InitOptions{
+		ProtocolVersion: common.ProtocolVersion,
+		KeyResourceID:   keyResourceID,
+		HashFunc:        hashFunc,
+	}
+	if ctxDeadline, ok := ctx.Deadline(); ok {
+		InitOptions.CtxDeadline = &ctxDeadline
+	}
+	pluginClient := newPluginClient(ctx, executable, InitOptions, makeCommand)
+	return pluginClient, nil
+}
---
+++ pkg/signature/kms/cliplugin/handler/dispatch.go
@@ -0,0 +1,53 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"github.com/sigstore/sigstore/pkg/signature/kms"
+	"github.com/sigstore/sigstore/pkg/signature/kms/cliplugin/common"
+)
+
+func GetPluginArgs(osArgs []string) (*common.PluginArgs, error) {
+	argsStr := osArgs[2]
+	var args common.PluginArgs
+	if err := json.Unmarshal([]byte(argsStr), &args); err != nil {
+		return nil, err
+	}
+	return &args, nil
+}
+
+func WriteResponse(wr io.Writer, resp *common.PluginResp) error {
+	enc, err := json.Marshal(resp)
+	if err != nil {
+		return err
+	}
+	fmt.Fprint(wr, string(enc))
+	return nil
+}
+
+func WriteErrorResponse(wr io.Writer, err error) {
+	resp := &common.PluginResp{
+		ErrorMessage: err.Error(),
+	}
+	WriteResponse(wr, resp)
+}
+
+func Dispatch(stdout io.Writer, stdin io.Reader, args *common.PluginArgs, impl kms.SignerVerifier) (*common.PluginResp, error) {
+	var resp common.PluginResp
+	var err error
+	switch args.Method {
+	case common.SupportedAlgorithmsMethodName:
+		resp.SupportedAlgorithms, err = SupportedAlgorithms(stdin, args.SuportedAlgorithms, impl)
+	case common.PublicKeyMethodName:
+		resp.PublicKey, err = PublicKey(stdin, args.PublicKey, impl)
+	case common.SignMessageMethodName:
+		resp.SignMessage, err = SignMessage(stdin, args.SignMessage, impl)
+	}
+	if err != nil {
+		resp.ErrorMessage = err.Error()
+	}
+	WriteResponse(stdout, &resp)
+	return &resp, err
+}
