8d3ab7e2012e97f3aa9c106ed420f83c3e6b8de1
Ramon Petgrave <ramon.petgrave64@gmail.com>
2025-01-28 14:43:07 -0500

    typo
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

--- test/cliplugin/localkms/implementation.go
+++ test/cliplugin/localkms/implementation.go
@@ -77,7 +77,7 @@ func (i LocalSignerVerifier) CreateKey(ctx context.Context, algorithm string) (c
 		return nil, fmt.Errorf("error generating private key: %w", err)
 	}
 
-	// wirte to the path
+	// wirite to the path
 	privateKeyPEM := &pem.Block{
 		Type:  "RSA PRIVATE KEY",
 		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
