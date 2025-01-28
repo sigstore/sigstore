fe793422be4f8097aa6251dc9b242c1c25f79c8d
Ramon Petgrave <ramon.petgrave64@gmail.com>
2025-01-27 19:16:29 -0500

    add ctx done check for CryptoSigner
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

--- pkg/signature/kms/cliplugin/signer.go
+++ pkg/signature/kms/cliplugin/signer.go
@@ -218,6 +218,9 @@ type CryptoSigner struct {
 
 // CryptoSigner returns a wrapper around PluginClient.
 func (c PluginClient) CryptoSigner(ctx context.Context, errFunc func(error)) (crypto.Signer, crypto.SignerOpts, error) {
+	if err := ctx.Err(); err != nil {
+		return nil, nil, err
+	}
 	return &CryptoSigner{
 		client:  &c,
 		ctx:     ctx,
