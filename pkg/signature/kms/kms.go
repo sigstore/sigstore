69687128039c36e5279d71183519dd3c928850b6
Ramon Petgrave <ramon.petgrave64@gmail.com>
2025-02-10 11:57:22 -0500

    typo
    Signed-off-by: Ramon Petgrave <ramon.petgrave64@gmail.com>

--- pkg/signature/kms/kms.go
+++ pkg/signature/kms/kms.go
@@ -29,7 +29,7 @@ import (
 
 // ProviderNotFoundError indicates that no matching KMS provider was found
 //
-// Deprecated: ProviderNotFoundError is no longer rerturned by Get(). If no matching provider is found
+// Deprecated: ProviderNotFoundError is no longer returned by Get(). If no matching provider is found
 // Get(), attempts to find a plugin program on the system, returning exec.ErrNotFound if not found.
 type ProviderNotFoundError struct {
 	ref string
