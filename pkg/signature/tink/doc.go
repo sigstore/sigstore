c049f8ddc6000381f00e7a676875c2194ed38fe4
Colleen Murphy <cmurphy@users.noreply.github.com>
2025-03-11 11:30:26 -0700

    Add tink package (#2024)
    Copy in the tink package from Rekor v1 so that it can be used by other
    services.
    
    Signed-off-by: Colleen Murphy <colleenmurphy@google.com>

---
+++ pkg/signature/tink/doc.go
@@ -0,0 +1,17 @@
+// Copyright 2025 The Sigstore Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package tink contains utilities for interacting with Tink.
+// This was originally copied from https://github.com/sigstore/rekor/blob/c820fcaf3afdc91f0acf6824d55c1ac7df249df1/pkg/signer/tink/tink.go.
+package tink
